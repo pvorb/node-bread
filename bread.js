e7cd6e1c17f849eee4e9a87759b14657ab3e2701
Paul Vorbach <paul@vorb.de>
2011-12-09 16:58:53 +0100

    Fix endentation

--- bread.js
+++ bread.js
@@ -6,53 +6,53 @@ var bake = require("bake"),
 // Main function
 var bread = function(conf) {
 
-	// later holds a reference to the Index object
-	var index;
+  // later holds a reference to the Index object
+  var index;
 
-	// Initialize hooks.
-	var hooks = {};
+  // Initialize hooks.
+  var hooks = {};
 
-	// Parse contents with `marked()`
-	hooks.__content = function(fn, prop) {
-		return marked(prop.__content);
-	};
+  // Parse contents with `marked()`
+  hooks.__content = function(fn, prop) {
+    return marked(prop.__content);
+  };
 
-	// Parse ISO 8601 dates
-	hooks.date = function(fn, prop) {
-		var date;
-		// If it's not a date, try parse it
-		if (typeof prop.date !== "date") {
-			try {
-				date = isodate(prop.date);
-			} catch (err) {
-				date = new Date(prop.date);
-			}
-		} else {
-			date = prop.date;
-		}
-		return date;
-	};
+  // Parse ISO 8601 dates
+  hooks.date = function(fn, prop) {
+    var date;
+    // If it's not a date, try parse it
+    if (typeof prop.date != "date") {
+      try {
+        date = isodate(prop.date);
+      } catch (err) {
+        date = new Date(prop.date);
+      }
+    } else {
+      date = prop.date;
+    }
+    return date;
+  };
 
-	// Add files to index
-	hooks.__writeAfter = function(fn, prop) {
-		index.add(prop);
-	};
+  // Add files to index
+  hooks.__writeAfter = function(fn, prop) {
+    index.add(prop);
+  };
 
-	// Write indexes, feeds and tag pages
-	hooks.__complete = function(fn, prop) {
-		if (conf.indexes)
-			index.write(conf);
-		if (conf.tags)
-		 	index.writeTags(conf);
-	};
+  // Write indexes, feeds and tag pages
+  hooks.__complete = function(fn, prop) {
+    if (conf.indexes)
+      index.write(conf);
+    if (conf.tags)
+      index.writeTags(conf);
+  };
 
-	new Index(conf.dbinf, function(i) {
-		// save ref to index
-		index = i;
+  new Index(conf.dbinf, function(i) {
+    // save ref to index
+    index = i;
 
-		// bake some bread
-		bake(conf, hooks);
-	});
+    // bake some bread
+    bake(conf, hooks);
+  });
 };
 
 module.exports = bread;
--- test/test.js
+++ test/test.js
@@ -1,46 +1,46 @@
 var bread = require("../");
 
 var conf = {
-	dbinf: {
-		host: "localhost",
-		port: "27017",
-		name: "index",
-		collection: "pages"
-	},
-	directories: {
-		input: "pub",
-		output: "pub",
-		templates: "tpl"
-	},
-	fileExtensions: {
-		"mkd": "html",
-		"md": "html",
-		"mdown": "html",
-		"markdown": "html"
-	},
-	indexes: [
-		{
-			title: "Blog",
-			pattern: /\/\d\/[^\/]+/,
-			path: {
-				first: "index.html",
-				pattern: "index-{{page}}.html"
-			},
-			template: "index.tpl",
-			limit: 2,
-			sort: [["date", "desc"]]
-		},
-		{
-			title: "Blog",
-			path : "feed.xml",
-			template: "atom.tpl",
-			limit: 10,
-			sort: [["date", "desc"]]
-		}
-	],
-	properties: {
-		siteTitle: "My Site"
-	}
+  dbinf: {
+    host: "localhost",
+    port: "27017",
+    name: "index",
+    collection: "pages"
+  },
+  directories: {
+    input: "pub",
+    output: "pub",
+    templates: "tpl"
+  },
+  fileExtensions: {
+    "mkd": "html",
+    "md": "html",
+    "mdown": "html",
+    "markdown": "html"
+  },
+  indexes: [
+    {
+      title: "Blog",
+      pattern: /\/\d\/[^\/]+/,
+      path: {
+        first: "index.html",
+        pattern: "index-{{page}}.html"
+      },
+      template: "index.tpl",
+      limit: 2,
+      sort: [["date", "desc"]]
+    },
+    {
+      title: "Blog",
+      path : "feed.xml",
+      template: "atom.tpl",
+      limit: 10,
+      sort: [["date", "desc"]]
+    }
+  ],
+  properties: {
+    siteTitle: "My Site"
+  }
 };
 
 bread(conf);
