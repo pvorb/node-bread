ddf3911cd698f89e6d1784cd8a180f97f7d198d4
Paul Vorbach <paul@vorb.de>
2012-02-29 00:40:05 +0100

    Drop collection before baking

--- bread.js
+++ bread.js
@@ -27,84 +27,89 @@ var bread = function(conf, cb) {
     if (err)
       return cb(err);
 
-    // Open collection
-    db.collection(conf.db.collection, function (err, collection) {
+    db.dropCollection(conf.db.collection, function (err) {
       if (err)
         return cb(err);
 
-      // Get registry
-      new Reg(collection, function (err, reg) {
+      // Open collection
+      db.collection(conf.db.collection, function (err, collection) {
         if (err)
           return cb(err);
 
-        var hooks = {};
-        reg.tags = new Set();
+        // Get registry
+        new Reg(collection, function (err, reg) {
+          if (err)
+            return cb(err);
 
-        // Parse contents with `marked()`
-        hooks.__content = function __content(f, prop, cb) {
-          pandoc(prop.__content, 'markdown', 'html', cb);
-        };
+          var hooks = {};
+          reg.tags = new Set();
 
-        hooks.__propBefore = function __propBefore(f, prop, cb) {
-          if (!prop.modified)
-            prop.modified = prop.created;
+          // Parse contents with `marked()`
+          hooks.__content = function __content(f, prop, cb) {
+            pandoc(prop.__content, 'markdown', 'html', cb);
+          };
 
-          cb(null, prop);
-        };
+          hooks.__propBefore = function __propBefore(f, prop, cb) {
+            if (!prop.modified)
+              prop.modified = prop.created;
 
-        function fixDate(date, cb) {
-          var created;
-          // If it's not a date, try to parse it
-          if (typeof date != 'date' && typeof date != 'undefined') {
-            try {
-              date = isodate(date);
-            } catch (err) {
+            cb(null, prop);
+          };
+
+          function fixDate(date, cb) {
+            var created;
+            // If it's not a date, try to parse it
+            if (typeof date != 'date' && typeof date != 'undefined') {
               try {
-                date = new Date(date);
+                date = isodate(date);
               } catch (err) {
-                return cb(err);
+                try {
+                  date = new Date(date);
+                } catch (err) {
+                  return cb(err);
+                }
               }
             }
+
+            cb(null, date);
           }
 
-          cb(null, date);
-        }
+          hooks.created = function created(f, prop, cb) {
+            fixDate(prop.created, cb);
+          };
 
-        hooks.created = function created(f, prop, cb) {
-          fixDate(prop.created, cb);
-        };
+          hooks.modified = function modified(f, prop, cb) {
+            fixDate(prop.modified, cb);
+          };
 
-        hooks.modified = function modified(f, prop, cb) {
-          fixDate(prop.modified, cb);
-        };
+          hooks.__writeAfter = function __writeAfter(f, prop, cb) {
+            var id = prop._id;
+            delete prop._id;
+            reg.extend(id, prop, x);
+            if (prop.tags)
+              for (var i = 0; i < prop.tags.length; i++)
+                reg.tags.add(prop.tags[i].trim());
+            cb(null, prop);
+          };
 
-        hooks.__writeAfter = function __writeAfter(f, prop, cb) {
-          var id = prop._id;
-          delete prop._id;
-          reg.extend(id, prop, x);
-          if (prop.tags)
-            for (var i = 0; i < prop.tags.length; i++)
-              reg.tags.add(prop.tags[i].trim());
-          cb(null, prop);
-        };
+          // Write indexes, feeds and tag pages
+          hooks.__complete = function(f, prop, cb) {
+            console.log('Content files written.');
+            write(reg, conf, function (err) {
+              if (err)
+                return cb(err);
+              cb(null);
+            });
+          };
 
-        // Write indexes, feeds and tag pages
-        hooks.__complete = function(f, prop, cb) {
-          console.log('Content files written.');
-          write(reg, conf, function (err) {
-            if (err)
-              return cb(err);
-            cb(null);
+          bake(conf, hooks, function (err) {
+            db.close();
+            cb(err);
           });
-        };
-
-        bake(conf, hooks, function (err) {
-          db.close();
-          cb(err);
-        });
-      });
-    });
-  });
+        }); // Registry
+      }); // collection
+    }); // dropCollection
+  }); // open
 };
 
 module.exports = bread;
